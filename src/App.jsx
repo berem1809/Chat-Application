610af39b38e59310c9fb986b1f9d8cd019b0f394
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-09-30 22:44:58 +0530

    First Update(Initial Stage)

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import List from './components/list/List'
+import Detail from './components/detail/Detail'
+import Chat from './components/chat/Chat'
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <List/>
+      <Chat/>
+      <Detail/>
+      
+
+    </div>
+  )
+}
+
+export default App
